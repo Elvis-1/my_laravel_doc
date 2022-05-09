1fbd66aa75acd4472180541dae968dff695c329d
Elvis <66243414+Elvis-1@users.noreply.github.com>
2022-05-09 16:32:11 +0100

    updated

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
 
-/** FORMATING DATE ON TABLE */
+/**  ON TABLE */
 /*
-date('l jS \\of F Y h:i:s A'), // Sunday 8th of May 2022 06:33:32 PM
+FORMATING DATE -->  date('l jS \\of F Y h:i:s A'), // Sunday 8th of May 2022 06:33:32 PM
+
+MAKING A SINGLE MIGRATION --> php artisan migrate --path database\migrations\2022_05_09_023917_alter_post_table.php
+
+CREATING A MODEL WITH MIGRATION --> php artisan make:model Testimony --migration
+
+CREATING A CONTROLLER WITH CRUD METHODS --> php artisan make:controller TestimonyController --api
 */
+
+/** SHARED SERVER */
+
+/*
+ TO REDIRECT LARAVEL SITE TO YOUR PUBLIC_HTML IN SHARED HOSTING USE
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+  /*  public function register()
+    {
+        $this->app->bind('path.public', function() {
+          return base_path().'/public_html';
+        });
+    }
+ */
+
+
